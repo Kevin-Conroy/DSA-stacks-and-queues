1b1b74d97642545eb9b097824a6528e5bd1b689a
Kevin Conroy <74108431+Kevin-Conroy@users.noreply.github.com>
2021-07-05 15:14:38 -0700

    initial commit

---
+++ stack-utils.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Stack = require('./stack');
+const utils = require('./stack-utils');
+const stack = new Stack();
+stack.push(6);
+stack.push(1);
+stack.push(3);
+stack.push(18);
+stack.push(4);
+stack.push(499);
+
+
+function sort(unsorted) {
+  const tempStack = new Stack();
+
+  let temp;
+  while (unsorted.top !== null) {
+    temp = unsorted.pop();
+    while (tempStack.top !== null && tempStack.top.value > temp) {
+      unsorted.push(tempStack.pop());
+    }
+    tempStack.push(temp);
+  }
+  return tempStack;
+}
+
+utils.display(stack);
+const sorted = sort(stack);
+utils.display(sorted);
